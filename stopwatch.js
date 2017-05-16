c5630826fe0d020d85caab371366e6d7cf935c37
AndreaxMoore <MooreA8957@sduhsd.lan>
2017-05-16 10:05:20 -0700

    Added Files

--- stopwatch.js
+++ stopwatch.js
@@ -3,8 +3,8 @@ var ss = document.getElementsByClassName('stopwatch');
 Get all elements that have a class of 'stopwatch'*/
 
 [].forEach.call(ss, function(s){
-    var currentTimer = 0,
-        interval = 0,
+    var currentTimer = 0, /*Current time in the timer starts at 0*/
+        interval = 0, /*Holding the set interval reference, keeps track of what's timing the timer*/
         LastUpdateTime = new Date().getTime(),
         start = s.querySelector('button.start'),
         stop = s.querySelector('button.stop'),
@@ -13,5 +13,19 @@ Get all elements that have a class of 'stopwatch'*/
         seconds = s.querySelector('span.seconds'),
         centiseconds = s.querySelector('span.centiseconds');
         /*The querySelector() method returns the first child element that matches a specified CSS selector(s) of an element.*/
-        });
+    function pad(n)/*helper function, it takes a number and returns a string that is two characters long. eg: 1 return 01*/ {
+        return ('00' + n).substr(-2); /*extract the last two characters off of that string*/
+    }
+
+    function update(){
+        var now = new Date().getTime(),
+            dt = now - lastUpdateTime;
+        currentTimer += dt;
+        var time = new Date(currentTimer);
+        minutes.innerHTML = pad(time.getMinutes());
+        seconds.innerHTML = pad(time.getSeconds());
+        centiseconds.innerHTML = pad(time.getMilliseconds()/10);
+        lastUpdateTime = now;
+    }
+    });
 
